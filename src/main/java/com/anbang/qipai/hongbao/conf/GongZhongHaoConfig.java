4770d86fa89d0b5f6496f74bdd529a238c335874
林少聪 < @PC-20180515PRDG>
2019-02-12 10:07:27 +0800

    bugfix

---
+++ src/main/java/com/anbang/qipai/hongbao/conf/GongZhongHaoConfig.java
@@ -0,0 +1,8 @@
+package com.anbang.qipai.hongbao.conf;
+
+public class GongZhongHaoConfig {
+	public static String JSAPI_TICKET = "";
+	public static String ACCESSTOKEN = "";
+	public static final String APPID = " ";
+	public static final String APPSECRET = " ";
+}
--- src/main/java/com/anbang/qipai/hongbao/conf/IPVerifyConfig.java
+++ src/main/java/com/anbang/qipai/hongbao/conf/IPVerifyConfig.java
@@ -1,5 +1,5 @@
 package com.anbang.qipai.hongbao.conf;
 
 public class IPVerifyConfig {
-	public static String APPCODE = "f00ea85a042c4404acaa87c121fc0079";
+	public static String APPCODE = " ";
 }
